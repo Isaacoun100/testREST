bc69442ab0a281bdb247db5ed0cd52bfec8b0e28
Isaacoun100 <Isaacoun100@gmail.com>
2022-09-18 22:15:21 -0600

    JSON Management added

---
+++ FileManager/JSONManager.cs
@@ -0,0 +1,16 @@
+﻿namespace REST_API_MECATEC.FileManager; 
+
+using System.Text.Json;
+
+public class JSONManager {
+
+    public static void Main() { }
+
+    public JsonElement loadJSON(string text) {
+        JsonDocument doc = JsonDocument.Parse(text);
+        JsonElement root = doc.RootElement;
+        return root;
+    }
+    
+    
+}
